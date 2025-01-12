0049a70beb7915debd80f396c3aa35529b84b508
JackHunter2 <46602010+JackHunter2@users.noreply.github.com>
2025-01-12 22:57:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default ke port 5000 jika PORT kosong
+    app.run(debug=False, host='0.0.0.0', port=port)
